476fe21b79d425d1283a2fc09232cc27992266cf
m1880 <1@q.c>
2025-03-25 00:16:48 +0800

    基础性修改

--- Objects/oled_data_ascii.d
+++
@@ -1,3 +0,0 @@
-.\objects\oled_data_ascii.o: OLED\OLED_Data_ASCII.c
-.\objects\oled_data_ascii.o: OLED\OLED_Data.h
-.\objects\oled_data_ascii.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Objects/oled_data_image.d
+++
@@ -1,3 +0,0 @@
-.\objects\oled_data_image.o: OLED\OLED_Data_Image.c
-.\objects\oled_data_image.o: .\OLED\OLED_Data.h
-.\objects\oled_data_image.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
